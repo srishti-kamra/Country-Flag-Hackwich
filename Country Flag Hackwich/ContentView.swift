8dcba3f3cbc0e0527e476ab28d3a8763f35af0e2
Srishti Kamra <srishtikamra07@gmail.com>
2023-01-11 09:33:26 -0600

    Initial Commit

---
+++ Country Flag Hackwich/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Country Flag Hackwich
+//
+//  Created by Srishti Kamra  on 1/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Country Flag Hackwich/Country_Flag_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_HackwichApp.swift
+//  Country Flag Hackwich
+//
+//  Created by Srishti Kamra  on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
